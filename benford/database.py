1fc7738aa07ac1cc93e8cc4f53ba1d8e72264f08
nelsonlove <nelson.love@me.com>
2021-10-27 13:38:18 -0400

    Separate CSVFile model into models.py

--- benford/database.py
+++ benford/database.py
@@ -1,91 +1,14 @@
-import codecs
-import csv
-import io
-import itertools
-
-import chardet
 from flask_sqlalchemy import SQLAlchemy
-
-from benford.analysis import parse_numeric
 
 db = SQLAlchemy()
 
 
-class CSVFile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.now())
-    filename = db.Column(db.String(120), unique=True, nullable=False)
-    file = db.Column(db.LargeBinary, nullable=False)
-    encoding = db.Column(db.String(32), nullable=False)
-
-    @classmethod
-    def load(cls, data, filename):
-        # Detect encoding from sample
-        sample = data.read(10000)
-        encoding = chardet.detect(sample)['encoding']
-        data.seek(0)
-
-        csvfile = cls(
-            filename=filename,
-            file=data.read(),
-            encoding=encoding
-        )
-        db.session.add(csvfile)
-        db.session.commit()
-
-        return csvfile
-
-    def reader(self):
-        # Get dialect first
-        file = io.BytesIO(self.file)
-        sample = file.read(1024).decode(self.encoding)
-        file.seek(0)
-        dialect = csv.Sniffer().sniff(sample)
-
-        data_iterator = codecs.iterdecode(file, self.encoding)
-        return csv.reader(data_iterator, dialect)
-
-    def dump(self, start=0, end=None):
-        return itertools.islice(self.reader(), start, end)
-
-    def num_columns(self):
-        first_row = next(self.dump(end=1))
-        return len(first_row)
-
-    def data(self):
-        num_cols = self.num_columns()
-        return [row for row in self.dump() if len(row) == num_cols]
-
-    def unusable(self):
-        num_cols = self.num_columns()
-        return [row for row in self.dump() if len(row) != num_cols]
-
-    @property
-    def viable_columns(self):
-        def is_numeric_or_blank(x):
-            # We can skip blanks later, for now we just want to see if the parser breaks for other reasons
-            if not x:
-                return True
-            try:
-                parse_numeric(x)
-            except ValueError:
-                return False
-            return True
-
-        viable_columns = []
-
-        for i in range(self.num_columns()):
-            column = self.column(i)
-            # Start at col[1:] because we don't want to count possible headers
-            if all(is_numeric_or_blank(x) for x in column[1:]):
-                viable_columns.append((i, column[0]))
-
-        return viable_columns
-
-    def column(self, i):
-        if i >= self.num_columns():
-            raise IndexError
-        return [row[i] for row in self.data()]
-
-    def __len__(self):
-        return len([_ for _ in self.reader()])
+def init(app, db_path):
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI=f'sqlite:///{db_path}',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+    db.app = app
+    db.init_app(app)
+    db.create_all(app=app)
+    return app
---
+++ benford/models.py
@@ -0,0 +1,107 @@
+import codecs
+import csv
+import io
+import itertools
+
+import chardet
+
+from benford import analysis
+from benford.analysis import parse_numeric
+from benford.database import db
+
+
+class CSVFile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.now())
+    filename = db.Column(db.String(120), unique=True, nullable=False)
+    file = db.Column(db.LargeBinary, nullable=False)
+    encoding = db.Column(db.String(32), nullable=False)
+
+    @classmethod
+    def get_encoding(cls, data):
+        # Detect encoding from sample
+        sample = data.read(10000)
+        data.seek(0)
+        return chardet.detect(sample)['encoding']
+
+    def __init__(self, data, filename):
+        file = data.read()
+        data.seek(0)
+        super().__init__(
+            filename=filename,
+            file=file,
+            encoding=self.get_encoding(data)
+        )
+
+    def reader(self):
+        # Get dialect first
+        file = io.BytesIO(self.file)
+        sample = file.read(1024).decode(self.encoding)
+        file.seek(0)
+        dialect = csv.Sniffer().sniff(sample)
+
+        data_iterator = codecs.iterdecode(file, self.encoding)
+        return csv.reader(data_iterator, dialect)
+
+    def dump(self, start=0, end=None):
+        return itertools.islice(self.reader(), start, end)
+
+    def viable_rows(self):
+        data = list(self.dump())
+        return [row for row in data if len(row) == len(data[0])]
+
+    def __len__(self):
+        return len(list(self.dump()))
+
+    def viable_columns(self):
+        def is_numeric_or_blank(x):
+            # We can skip blanks later, for now we just want to see if the parser breaks for other reasons
+            if not x:
+                return True
+            try:
+                parse_numeric(x)
+            except ValueError:
+                return False
+            return True
+
+        data = self.viable_rows()
+        num_columns = len(data[0])
+        viable_columns = []
+
+        for i in range(num_columns):
+            column = [row[i] for row in data]
+            # Start at col[1:] because we don't want to count possible headers
+            if all(is_numeric_or_blank(x) for x in column[1:]):
+                viable_columns.append(i)
+
+        return viable_columns
+
+    def preview(self):
+        viable_rows = self.viable_rows()
+        num_rows = len(self)
+        num_preview_rows = min(len(viable_rows), 6)
+
+        return {
+            'numRows': num_rows,
+            'numDiscarded': num_rows - len(viable_rows),
+            'previewData': viable_rows[:num_preview_rows],
+            'viableColumnIndices': self.viable_columns()
+        }
+
+    def analysis(self, column_index):
+        viable_rows = self.viable_rows()
+        column = [row[column_index] for row in viable_rows]
+        data = analysis.clean_data(column)
+        expected = analysis.expected_distribution(len(data))
+        observed = analysis.observed_distribution(data)
+
+        return {
+            'name': viable_rows[0][column_index],
+            'index': column_index,
+            'n': len(data),
+            'expectedDistribution': expected,
+            'observedDistribution': observed,
+            'testStatistic': analysis.sum_chi_squares(expected, observed),
+            'criticalValues': analysis.CRITICAL_VALUES,
+            'goodnessOfFit': analysis.goodness_of_fit(expected, observed)
+        }
